404a11a5dbe2495149abe23a50b7316d7696065b
Jefferson Marchetti Ferreira <jefferson.marchetti@xpi.com.br>
2021-05-12 13:08:30 -0300

    Topzeira das galaxias

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # kafka-producer-rust-example
+
+### Documentação ainda é precária:
+
+**mas vamos lá...**  
+
+### Como Compilar
+
+```
+$ cargo update
+
+$ cargo build --release
+```
+
+### Como Executar
+
+```
+via Cargo Runtime
+$ cargo run "BROKER:PORTA" "TÓPICO" "MENSAGEM DO PAYLOAD" "A CHAVE DA MENSAGEM"
+
+nativo
+$ ./target/release/ultra_faster_producer "BROKER:PORTA" "TÓPICO" "MENSAGEM DO PAYLOAD" "A CHAVE DA MENSAGEM"
+```
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,30 @@ use rdkafka::config::ClientConfig;
 use rdkafka::producer::{BaseProducer, BaseRecord};
 use std::time::Duration;
 use chrono::{Local, DateTime};
+use std::env;
 
 fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    println!("ARGUMENTOS RECEBIDOS{:?}", args);
+
+    if args.len() < 5 {
+        return;
+    }
 
     let dt: DateTime<Local> = Local::now();
 
     println!("Finished pooling at {0}", dt);    
 
     let producer: BaseProducer = ClientConfig::new()    
-    .set("bootstrap.servers", "localhost:9092")
+    .set("bootstrap.servers", &args[1])
     .create()
     .expect("Producer creation error");
 
     producer.send(
-        BaseRecord::to("teste")
-            .payload("this is the payload")
-            .key("and this is a key"),
+        BaseRecord::to(&args[2])
+            .payload(&args[3])
+            .key(&args[4]),
     ).expect("Failed to enqueue");    
 
     // Poll at regular intervals to process all the asynchronous delivery events.
